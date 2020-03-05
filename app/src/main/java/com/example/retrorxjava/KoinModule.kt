ad3ece97a2d698386422b98146d3b36a1f131362
Eshwar <eswarpunnam@gmail.com>
2020-03-05 18:05:59 +0530

    Remove the Unwanted Code

--- app/src/main/java/com/example/retrorxjava/KoinModule.kt
+++ app/src/main/java/com/example/retrorxjava/KoinModule.kt
@@ -4,23 +4,14 @@ import com.example.retrorxjava.Network.Api
 import com.example.retrorxjava.utils.AppConfig.baseUrl
 import com.google.gson.GsonBuilder
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.schedulers.Schedulers.single
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.security.Provider
-import androidx.databinding.adapters.NumberPickerBindingAdapter.setValue
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.LiveData
-import com.example.retrorxjava.model.BookResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 
 val retrofitModule = module {
@@ -55,11 +46,7 @@ private fun retrofit(okHttpClient: OkHttpClient):Retrofit {
 
 }
 
-fun getProjectList(userId: String): LiveData<List<BookResponse>> {
-    val data = MutableLiveData<List<BookResponse>>()
-    //retrofit
-    return data
-}
+
 
 
 private fun provideForecastApi(retrofit: Retrofit): Api {
