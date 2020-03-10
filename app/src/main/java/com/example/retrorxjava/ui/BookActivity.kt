93be387af3a847675b16ba2fa896e7807e08bcaa
Eshwar <eswarpunnam@gmail.com>
2020-03-10 10:34:24 +0530

    create An Activity named as BookActivity

---
+++ app/src/main/java/com/example/retrorxjava/ui/BookActivity.kt
@@ -0,0 +1,57 @@
+package com.example.retrorxjava.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrorxjava.R
+import com.example.retrorxjava.helper.NetworkHelper
+import com.example.retrorxjava.model.Book
+import com.example.retrorxjava.model.BookResponse
+import com.example.retrorxjava.viewmodel.Apiresult
+import com.example.retrorxjava.viewmodel.BookViewModel
+import io.reactivex.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class BookActivity : AppCompatActivity(), Apiresult {
+    private val mainViewModel by viewModel<BookViewModel>()
+    private val adapter: BookAdapter by lazy { BookAdapter() }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book)
+
+        books_list.layoutManager = LinearLayoutManager(this)
+        books_list.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        books_list.setHasFixedSize(true)
+        books_list.adapter = adapter
+        mainViewModel.showBooksList1
+
+        when {
+            NetworkHelper.isNetworkConnected(this) -> mainViewModel.response(this)
+            else -> {
+                Toast.makeText(this, "No Internet!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
+    }
+    override fun onSucess(data: Any?) {
+         val bookResponse = data as BookResponse
+        setAdapter(bookResponse.items as List<Book>)
+
+
+    }
+
+    override fun onError(error: String?) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setAdapter(bookList: List<Book>) {
+        adapter.setList(bookList)
+        adapter.notifyDataSetChanged()
+    }
+}
