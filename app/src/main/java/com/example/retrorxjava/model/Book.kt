ed783409edfe6d187c31421e3b81c95b5d034c8e
Eshwar <eswarpunnam@gmail.com>
2020-03-10 10:35:08 +0530

    Initial Changes

--- app/src/main/java/com/example/retrorxjava/model/Book.kt
+++ app/src/main/java/com/example/retrorxjava/model/Book.kt
@@ -1,26 +1,13 @@
 package com.example.retrorxjava.model
 
-import com.google.gson.annotations.SerializedName
-
 data class Book(
-
-
-    @SerializedName("id")
     var id: String? = null,
-    @SerializedName("title")
     var title: String? = null,
-    @SerializedName("content_html")
     var content_html: String? = null,
-    @SerializedName("url")
     var url: String? = null,
-    @SerializedName("external_url")
     var external_url: String? = null,
-    @SerializedName("date_published")
     var date_published: String? = null,
-    @SerializedName("author")
     var author: String? = null
-
-
 
 )
 
