b64420edc031b53089257b7ce2ef278464dcb66c
Eshwar <eswarpunnam@gmail.com>
2020-03-12 19:25:55 +0530

    Crate the newheleper class for date formatting

---
+++ app/src/main/java/com/example/retrorxjava/helper/DateHelper.kt
@@ -0,0 +1,17 @@
+package com.example.retrorxjava.helper
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+
+object DateHelper {
+  @SuppressLint("SimpleDateFormat")
+  fun Date(date:String):String{
+
+      val newFormat = SimpleDateFormat("yyyy-MM-dd hh:mm")
+      val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+      val NormalDate = format.parse(date)
+      val formattedDate = newFormat.format(NormalDate!!)
+      return formattedDate
+  }
+
+}
