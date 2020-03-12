b9eb7fd28650f33c60e2098e4e0705c757e2de98
Eshwar <eswarpunnam@gmail.com>
2020-03-12 11:42:26 +0530

    Initial Changes

--- app/src/main/java/com/example/retrorxjava/ui/BookAdapter.kt
+++ app/src/main/java/com/example/retrorxjava/ui/BookAdapter.kt
@@ -2,14 +2,19 @@
 
 package com.example.retrorxjava.ui
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.text.Html
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.retrorxjava.databinding.ItemListBinding
 import com.example.retrorxjava.model.Book
 
 class BookAdapter(
+    var context: Context,
     var bookList: List<Book> = emptyList()
 
 ) : RecyclerView.Adapter<BookViewHolder>() {
@@ -17,7 +22,7 @@ class BookAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ItemListBinding.inflate(layoutInflater, parent, false)
-        return BookViewHolder(binding)
+        return BookViewHolder(binding, context)
 
 
     }
@@ -36,14 +41,36 @@ class BookAdapter(
     }
 }
 
-class BookViewHolder(val binding: ItemListBinding) : RecyclerView.ViewHolder(binding.root) {
-    val b = binding.bookHtml
+class BookViewHolder(val binding: ItemListBinding, val context: Context) :
+    RecyclerView.ViewHolder(binding.root) {
+    val b = binding.bookUrl
+    val c = binding.bookHtml
+    val title = binding.bookTitle
+    val comment = binding.externalUrl
+    val id = binding.bookId
 
 
     fun bindModel(it: Book) {
         binding.book = it
-        b.setText(Html.fromHtml(Html.fromHtml(it.content_html).toString()))
+        c.setText(Html.fromHtml(Html.fromHtml(it.content_html!!.removeRange(1, 31)).toString()))
+        title.setOnClickListener {
+            url(b)
 
+        }
+        comment.setOnClickListener {
+
+            url(id)
+        }
+
+
+    }
+
+
+    private fun url(text: TextView) {
+        val url = text.getText().toString()
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        context.startActivity(i)
     }
 
 }
