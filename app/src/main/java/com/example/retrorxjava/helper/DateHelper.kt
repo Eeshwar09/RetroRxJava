5cd8850d71e57de735f3f4f5f604352a31536655
Eshwar <eswarpunnam@gmail.com>
2020-03-16 11:22:21 +0530

    initial changes

--- app/src/main/java/com/example/retrorxjava/helper/DateHelper.kt
+++ app/src/main/java/com/example/retrorxjava/helper/DateHelper.kt
@@ -7,7 +7,7 @@ object DateHelper {
   @SuppressLint("SimpleDateFormat")
   fun Date(date:String):String{
 
-      val newFormat = SimpleDateFormat("yyyy-MM-dd hh:mm")
+      val newFormat = SimpleDateFormat("yyyy-MM-dd")
       val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
       val NormalDate = format.parse(date)
       val formattedDate = newFormat.format(NormalDate!!)
