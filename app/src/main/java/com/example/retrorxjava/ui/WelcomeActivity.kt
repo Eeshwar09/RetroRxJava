c89a83f0fb730d23eb483df4fd7e4823065f994f
Eshwar <eswarpunnam@gmail.com>
2020-03-16 11:11:52 +0530

    initial changes

--- app/src/main/java/com/example/retrorxjava/ui/WelcomeActivity.kt
+++ app/src/main/java/com/example/retrorxjava/ui/WelcomeActivity.kt
@@ -1,46 +1,38 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.retrorxjava.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.os.HandlerThread
 import com.example.retrorxjava.R
-import android.widget.ProgressBar
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.content.Intent
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.animation.ObjectAnimator
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import kotlinx.android.synthetic.main.activity_welcome.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 
 class WelcomeActivity : AppCompatActivity() {
 
-    var splashProgress: ProgressBar? = null
-    var SPLASH_TIME = 5000
+    private var plashTime = 4000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-        splashProgress = findViewById(R.id.splashProgress);
-        playProgress();
+        playProgress()
         toolbar.title = getString(R.string.app_name)
 
 
         Handler().postDelayed({
-            val mySuperIntent = Intent(this,BookActivity::class.java)
+            val mySuperIntent = Intent(this, BookActivity::class.java)
             startActivity(mySuperIntent)
-
             finish()
-        }, SPLASH_TIME.toLong())
+        }, plashTime.toLong())
 
     }
+
     private fun playProgress() {
         ObjectAnimator.ofInt(splashProgress, "progress", 100)
-            .setDuration(5000)
+            .setDuration(4000)
             .start()
     }
 }
