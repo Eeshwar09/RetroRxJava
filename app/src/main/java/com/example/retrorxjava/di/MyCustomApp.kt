97bf60a9b6873d00a50791b2e1baadcf1c0e8623
Eshwar <eswarpunnam@gmail.com>
2020-03-16 19:31:44 +0530

    remove the unnecessary code

--- app/src/main/java/com/example/retrorxjava/di/MyCustomApp.kt
+++ app/src/main/java/com/example/retrorxjava/di/MyCustomApp.kt
@@ -1,7 +1,6 @@
 package com.example.retrorxjava.di
 
 import android.app.Application
-import com.example.retrorxjava.di.retrofitModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
