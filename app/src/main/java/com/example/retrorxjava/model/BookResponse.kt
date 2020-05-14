dad87a5d935841cf6632f74131f06c1991adf682
Eshwar <eswarpunnam@gmail.com>
2020-05-14 20:43:48 +0530

    write the List<Book> insted od MutableList<Book>

--- app/src/main/java/com/example/retrorxjava/model/BookResponse.kt
+++ app/src/main/java/com/example/retrorxjava/model/BookResponse.kt
@@ -1,9 +1,11 @@
 package com.example.retrorxjava.model
 
+import java.io.Serializable
+
 data class BookResponse(
     var version: String? = null,
     var title: String? = null,
     var description: String? = null,
     var home_page_url: String? = null,
-    var items: MutableList<Book>
-)
+    var items: List<Book>? = null
+): Serializable
