0cec334403bcbf1825f0c466076e99f8f920b8fb
Eshwar <eswarpunnam@gmail.com>
2020-05-14 20:39:33 +0530

    Initial changes

--- app/src/main/java/com/example/retrorxjava/network/Api.kt
+++ app/src/main/java/com/example/retrorxjava/network/Api.kt
@@ -2,6 +2,7 @@ package com.example.retrorxjava.network
 
 import com.example.retrorxjava.model.BookResponse
 import io.reactivex.Observable
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface Api {
@@ -9,3 +10,4 @@ interface Api {
     @GET("frontpage.jsonfeed")
     fun getMovies() : Observable<BookResponse>
 }
+//https://hnrss.org/frontpage.jsonfeed
