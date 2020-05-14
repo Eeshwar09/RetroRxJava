ea1ca9b4f543b52beceec3c8e7007dda4bbedc4c
Eshwar <eswarpunnam@gmail.com>
2020-05-14 20:42:34 +0530

    initial changes in viewholder

--- app/src/main/java/com/example/retrorxjava/ui/BookAdapter.kt
+++ app/src/main/java/com/example/retrorxjava/ui/BookAdapter.kt
@@ -5,6 +5,7 @@ package com.example.retrorxjava.ui
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,9 @@ import com.example.retrorxjava.databinding.ItemListBinding
 import com.example.retrorxjava.helper.DateHelper
 import com.example.retrorxjava.model.Book
 import java.util.ArrayList
+import com.google.firebase.firestore.auth.User
+
+
 
 
 @Suppress("UNCHECKED_CAST")
@@ -19,7 +23,7 @@ class BookAdapter(
     private var context: Context,
     private var bookList: List<Any> = emptyList()
 
-) : RecyclerView.Adapter<BookViewHolder>() {
+) : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -36,34 +40,43 @@ class BookAdapter(
     fun setList(list: List<Book>) {
         this.bookList = list
         notifyDataSetChanged()
+
     }
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
-        holder.bindModel(bookList[position] as Book, bookList as ArrayList<Book>)
+        holder.bindModel(bookList[position] as Book)
     }
-}
-
-@Suppress("CAST_NEVER_SUCCEEDS")
-class BookViewHolder(private val binding: ItemListBinding, private val context: Context) :
-    RecyclerView.ViewHolder(binding.root) {
-    private val date = binding.datepublished
 
 
-    @SuppressLint("SimpleDateFormat")
-    fun bindModel(it: Book, bookList: ArrayList<Book>) {
-        binding.book = it
-        val datePublished = DateHelper.Date(it.date_published.toString())
-        date.text = datePublished
+    @Suppress("CAST_NEVER_SUCCEEDS")
+    class BookViewHolder(private val binding: ItemListBinding, private val context: Context) :
+        RecyclerView.ViewHolder(binding.root) {
+        private val date = binding.datepublished
 
-        itemView.setOnClickListener {
-            val pos = adapterPosition
-            val intent = Intent(context, ArticleActivity::class.java)
-            intent.putExtra("Position", pos)
-            intent.putExtra("BookList", bookList)
-            context.startActivity(intent)
+
+        @SuppressLint("SimpleDateFormat", "CommitPrefEdits")
+        fun bindModel( it: Book) {
+            binding.book = it
+           val bookList = listOf(it)
+            val userCollection = HashSet<Book>(bookList)
+
+            val userList = ArrayList<Book>(userCollection)
+            val datePublished = DateHelper.Date(it.date_published.toString())
+            date.text = datePublished
+
+            itemView.setOnClickListener {
+                val pos = adapterPosition
+                val intent = Intent(context, ArticleActivity::class.java)
+                intent.putExtra("Position", pos)
+                intent.putExtra("BookList", userList)
+
+
+
+                context.startActivity(intent)
+            }
+
         }
 
+
     }
-
-
 }
